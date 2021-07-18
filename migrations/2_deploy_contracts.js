8b702ab591ed69e4a218bfb6908ebd019601df9f
Bryson Palmer <brysonpalmer@gmail.com>
2021-07-18 14:52:09 -0700

    Created token_farm folder, created repo, cloned repo, and created a react app. I populated files from the defi_tutorial from dappuniversity https://github.com/dappuniversity/defi_tutorial so I could push updates to my repo. Setting up initial project.

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,6 @@
+// eslint-disable-next-line no-undef
+const TokenFarm = artifacts.require('TokenFarm')
+
+module.exports = function(deployer) {
+  deployer.deploy(TokenFarm);
+}
---
+++ src/contracts/TokenFarm.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.5.0; 
+
+contract TokenFarm {
+
+    // All code goes in here...
+    string public name = "Dapp Token Farm";
+
+}
